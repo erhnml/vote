ec8ccd5cac54fb934370ad7e20cb2f09533ba318
namallerhan@gmail.com <namallerhan@gmail.com>
2021-01-05 19:51:30 +0300

    add: routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Detail from '../pages/detail';
+import List from '../pages/list';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <Switch>
+        <Route path="/detail/:id" component={Detail} />
+            <Route path="/" component={List} />
+      </Switch>
+    </Router>
   );
 }
 
