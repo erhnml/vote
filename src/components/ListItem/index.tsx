2455c404f255cea066fe89ca0c7b1fa75683633d
namallerhan@gmail.com <namallerhan@gmail.com>
2021-01-06 23:00:33 +0300

    add: list item hover event

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import {Link} from 'react-router-dom';
 
@@ -14,9 +14,20 @@ export interface ListItemProps  {
 
 const ListItem = ({post}: ListItemProps) => {
   const {id, name, link, vote} = post;
+  const [hover ,setHover] = useState(false)
   const handleVote = () => alert('Voted')
+  const handleMouseEvet = (event:string) => {
+    console.log(event)
+  }
+
+ 
+
   return (
-    <Wrapper >
+    <Wrapper 
+      onMouseOver={() =>setHover(true)}
+      onMouseLeave={() =>setHover(false)}
+      hover={hover}
+    >
       <Content to={`/detail/${id}`}>
         <VoteCount>
           <span>{vote}</span>
@@ -43,9 +54,12 @@ const Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   height: 60px;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
   border-bottom: 1px solid #c4c3c3;
   padding-bottom: 10px;
+  background-color: ${({ hover }:{hover: boolean}) => hover ? 'rgba(234, 91, 12, 0.3)' : 'transparent'};
+  cursor: pointer;
+  padding: 10px 10px;
 `;
 const Content = styled(Link)`
   display: flex;
@@ -87,6 +101,7 @@ const Vote = styled.div`
   margin-left: 15px;
   align-items: center;
   cursor: pointer;
+  pointer-events: click;
 `;
 
 export default ListItem;
